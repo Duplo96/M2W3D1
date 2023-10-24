277f23a852b85574418cfacce74fb7b848826dcf
Tiziano Titone <titone1996@gmail.com>
2023-10-25 00:22:58 +0200

    Update Function Javascript.js

--- Function Javascript.js
+++ Function Javascript.js
@@ -37,30 +37,59 @@ console.log(
 );
 console.log()
 /* SCRIVI QUI LA TUA RISPOSTA */
-let reverseString = function (text) {
-    return text.split("").reverse().join("")
+// let reverseString = function (text) {
+// return text.split("").reverse().join("")
+// }
+// let stringaDaInvertire = "EPICODE" // CAMBIARE IL VALORE CON UNA PAROLA DA INVERTIRE //
+// let stringaInverita = "STRINGA INVERTITA = " + reverseString(stringaDaInvertire)
+// console.log(stringaInverita)
+
+let reverseString = function(text) {
+  let stringa = "";
+  for(let i =text.length -1;i>= 0;i--) {
+    stringa += text[i];
+  }
+  return stringa;
 }
-
-let stringaDaInvertire = "EPICODE" // CAMBIARE IL VALORE CON UNA PAROLA DA INVERTIRE //
-let stringaInverita = "STRINGA INVERTITA = " + reverseString(stringaDaInvertire)
-console.log(stringaInverita)
-
+let text = "EPICODE";
+let stringaInvertita = reverseString(text);
+console.log(stringaInvertita);
+console.log()
 /* ESERCIZIO 4
  Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.
 */
 /* SCRIVI QUI LA TUA RISPOSTA */
 let upperFirst = function (text) {
-
-
+let parola  = text.split(" ")
+for (let i = 0; i < parola.length; i++) {
+ let lettera = parola [i]
+ parola[i]= lettera.charAt(0).toUpperCase() + lettera.slice(1)
+}
+return parola.join("")
 };
-
+let parolaFinale = upperFirst("pippo franco")
+console.log(parolaFinale)
 /* ESERCIZIO 5
  Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 console.log()
-let giveMeRandom = function () {};
+let giveMeRandom = function (n) {
+let array = []
+for (let i = 0; i < n ; i++){
+  let randomnumber = Math.round(Math.random() *11); 
+  array.push(randomnumber)
+}
+return array
+};
+let result = giveMeRandom(7)
+console.log(result)
+
+
+
+
+
 console.log("//EXTRA:")
 console.log()
 console.log("ESERCIZIO 1 Scrivi una funzione chiamata area che riceve due parametri l1, l2 e calcola l'area del rettangolo associato.")
